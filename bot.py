e552ec92c2d8b23d5ff90ac9ffe7b6d7583272de
Rodrigo Valla <rodrigovalla@protonmail.ch>
2023-04-19 07:00:16 -0300

    downgrading to telegram-python-bot-13

--- bot.py
+++ bot.py
@@ -13,36 +13,35 @@ us = Usage("usage.csv")
 msg = Messages()
 pcs = PCS()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def start(update, context):
 	id = update.effective_chat.id
 	logging.info(str(hide_id(id)) + " started the bot...")
 	us.add_start()
-	await context.bot.send_message(chat_id=id, text=msg.get_message("hello", get_language(id)), parse_mode=ParseMode.HTML)
+	context.bot.send_message(chat_id=id, text=msg.get_message("hello", get_language(id)), parse_mode=ParseMode.HTML)
 
-async def get_pcs_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def get_pcs_info(update, context):
 	id = update.effective_chat.id
 	logging.info(str(hide_id(id)) + " ask for a pcs...")
 	text = update.message.text
 	try:
 		cardinality, ordinal, interval, is_inverted, z_pair, states, ordered_form, prime_form = pcs.get_set_info(text[5:])
 		vector = pcs.interval_vector(ordered_form)
-		await context.bot.send_message(chat_id=id, text=msg.get_message("pcs_1", get_language(id)), parse_mode=ParseMode.HTML)	
 		if not ordinal == None:
 			m = msg.build_pcs_message(cardinality, ordinal, interval, is_inverted, z_pair, states, ordered_form, prime_form, vector, get_language(id))
 			us.add_pcs(0)
-			await context.bot.send_message(chat_id=id, text=m, parse_mode=ParseMode.HTML)
+			context.bot.send_message(chat_id=id, text=m, parse_mode=ParseMode.HTML)
 		else:
 			us.add_pcs(1)
-			await context.bot.send_message(chat_id=id, text=msg.get_message("pcs_2", get_language(id)), parse_mode=ParseMode.HTML)
+			context.bot.send_message(chat_id=id, text=msg.get_message("pcs_2", get_language(id)), parse_mode=ParseMode.HTML)
 	except:
 		us.add_pcs(2)
-		await context.bot.send_message(chat_id=id, text=msg.get_message("pcs_3", get_language(id)), parse_mode=ParseMode.HTML)	
+		context.bot.send_message(chat_id=id, text=msg.get_message("pcs_3", get_language(id)), parse_mode=ParseMode.HTML)	
 
-async def print_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def print_help(update, context):
 	id = update.effective_chat.id
 	logging.info(str(hide_id(id)) + " asked for help...")
 	us.add_help()
-	await context.bot.send_message(chat_id=id, text=msg.build_help_message(get_language(id)), parse_mode=ParseMode.HTML)
+	context.bot.send_message(chat_id=id, text=msg.build_help_message(get_language(id)), parse_mode=ParseMode.HTML)
 
 def get_language(id):
 	if id in en_users:
@@ -50,60 +49,60 @@ def get_language(id):
 	else:
 		return 0
 
-async def select_language(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def select_language(update, context):
 	id = update.effective_chat.id
 	logging.info(str(hide_id(id)) + " will set language...")
 	keyboard = [[InlineKeyboardButton(text="Español", callback_data="l_0"),
 				InlineKeyboardButton(text="English", callback_data="l_1")]]
 	reply = InlineKeyboardMarkup(keyboard)
-	await context.bot.send_message(chat_id=id, text=msg.get_message("language", get_language(id)), reply_markup=reply, parse_mode=ParseMode.HTML)
+	context.bot.send_message(chat_id=id, text=msg.get_message("language", get_language(id)), reply_markup=reply, parse_mode=ParseMode.HTML)
 
-async def set_language(update: Update, context: ContextTypes.DEFAULT_TYPE, query) -> None:
+def set_language(update, context):
 	id = update.effective_chat.id
 	if query == "l_1":
 		logging.info("English is the language selected by " + str(hide_id(id)))
 		en_users.add(id)
 		us.add_language(1)
-		await context.bot.send_message(chat_id=id, text=msg.get_message("language2", get_language(id)), parse_mode=ParseMode.HTML)
+		context.bot.send_message(chat_id=id, text=msg.get_message("language2", get_language(id)), parse_mode=ParseMode.HTML)
 	else:
 		logging.info("Spanish is the language selected by " + str(hide_id(id)))
 		en_users.discard(id)
 		us.add_language(0)
-		await context.bot.send_message(chat_id=id, text=msg.get_message("language3", get_language(id)), parse_mode=ParseMode.HTML)
+		context.bot.send_message(chat_id=id, text=msg.get_message("language3", get_language(id)), parse_mode=ParseMode.HTML)
 
-async def button_click(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def button_click(update, context):
 	query = update.callback_query
 	query.answer()
 	if query.data.startswith("l"):
-		await set_language(update, context, query.data)
+		set_language(update, context, query.data)
 
 #Sending usage data...
-async def bot_usage(update, context):
+def bot_usage(update, context):
 	id = update.effective_chat.id
 	m = update.message.text.split(" ")
 	if len(m) > 1 and m[1] == config["password"]:
 		m = us.build_usage_message()
-		await context.bot.send_message(chat_id=id, text=m, parse_mode=ParseMode.HTML)
+		context.bot.send_message(chat_id=id, text=m, parse_mode=ParseMode.HTML)
 	else:
 		logging.info(hide_id(id) + " wanted to check bot usage data...")
-		await context.bot.send_message(chat_id=id, text=msg.get_message("intruder", get_language(id)), parse_mode=ParseMode.HTML)
+		context.bot.send_message(chat_id=id, text=msg.get_message("intruder", get_language(id)), parse_mode=ParseMode.HTML)
 
 #Saving usage data...
-async def save_usage(update, context):
+def save_usage(update, context):
 	id = update.effective_chat.id
 	m = update.message.text.split(" ")
 	if len(m) > 1 and m[1] == config["password"]:
 		us.save_usage()
-		await context.bot.send_message(chat_id=id, text="Datos guardados...", parse_mode=ParseMode.HTML)
+		context.bot.send_message(chat_id=id, text="Datos guardados...", parse_mode=ParseMode.HTML)
 	else:
 		logging.info(hide_id(id) + " wanted to save bot usage data...")
-		await context.bot.send_message(chat_id=id, text=msg.get_message("intruder", get_language(id)), parse_mode=ParseMode.HTML)
+		context.bot.send_message(chat_id=id, text=msg.get_message("intruder", get_language(id)), parse_mode=ParseMode.HTML)
 
-async def error_notification(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+def error_notification(update, context):
 	id = update.effective_chat.id
 	m = "An error ocurred! While comunicating with chat " + str(hide_id(id))
 	logging.info(m)
-	await context.bot.send_message(chat_id=config["admin_id"], text=m, parse_mode=ParseMode.HTML)
+	context.bot.send_message(chat_id=config["admin_id"], text=m, parse_mode=ParseMode.HTML)
 
 #Hiding the first numbers of a chat id for the log...
 def hide_id(id):
