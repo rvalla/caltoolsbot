41676d9300a85738990697c21ccad1934ce96966
Rodrigo Valla <rodrigovalla@protonmail.ch>
2025-10-14 23:07:37 -0300

    New Users() class, improving Usage()...

--- usage.py
+++ usage.py
@@ -1,24 +1,29 @@
-import datetime as dt
+from datetime import datetime as dt, timedelta
 
 class Usage():
 	"The class to save usage data..."
 
 	def __init__(self, usage_path, errors_path):
+		self.date = dt.today()
 		self.output_path = usage_path
 		self.errors_path = errors_path
 		self.reset()
 
 	#Resseting data variables...
 	def reset(self):
-		self.last_save = dt.datetime.now() #the start up time...
+		self.last_save = dt.now().replace(hour=0, minute=0, second=0, microsecond=0)
 		self.start = 0
 		self.pcs = [0,0,0] #success, wrong set, wrong input...
-		self.chain = [0,0] #success, error...
+		self.chain = [0,0] #success, errors...
+		self.matrix = [0,0,0] #new matrices, operations, errors...
+		self.random = [0,0] #success, errors...
 		self.error_reports = 0
-		self.language = [0,0] #spanish, english...
+		self.language = [0,0,0] #spanish, english, chinese...
+		self.privacy = 0
 		self.help = 0
 		self.outofcontext = 0
 		self.errors = 0
+		self.admin = 0
 
 	#Building usage information message...
 	def build_usage_message(self):
@@ -26,75 +31,120 @@ class Usage():
 			"start: " + str(self.start) + "\n" + \
 			"pcs: " + str(self.pcs) + "\n" + \
 			"chain: " + str(self.chain) + "\n" + \
+			"matrix: " + str(self.matrix) + "\n" + \
+			"random: " + str(self.random) + "\n" + \
 			"error reports: " + str(self.error_reports) + "\n" + \
 			"language: " + str(self.language) + "\n" + \
+			"privacy: " + str(self.privacy) + "\n" + \
 			"help: " + str(self.help) + "\n" + \
 			"out of context: " + str(self.outofcontext) + "\n" + \
-			"errors: " + str(self.errors) + "\n"
+			"errors: " + str(self.errors) + "\n" + \
+			"admin: " + str(self.admin) + "\n"
 		return m
 
 	#Saving usage to file...
 	def save_usage(self):
 		file = open(self.output_path, "a")
-		t = dt.datetime.now()
-		i = t - self.last_save
+		t = dt.now()
 		date = str(t.year) + "-" + str(t.month) + "-" + str(t.day)
-		interval = str(i).split(".")[0]
-		line = self.build_usage_line(date, interval)
+		line = self.build_usage_line(date)
 		file.write(line)
 		file.close()
 		self.reset()
 
 	#Building a data line to save...
-	def build_usage_line(self, date, interval):
+	def build_usage_line(self, date):
 		line = date + ";"
-		line += interval + ";"
 		line += str(self.start) + ";"
-		line += str(self.pcs) + ";"
-		line += str(self.chain) + ";"
+		line += self.list_to_csv_format(self.pcs)
+		line += self.list_to_csv_format(self.chain)
+		line += self.list_to_csv_format(self.matrix)
+		line += self.list_to_csv_format(self.random)
 		line += str(self.error_reports) + ";"
-		line += str(self.language) + ";"
+		line += self.list_to_csv_format(self.language)
+		line += str(self.privacy) + ";"
 		line += str(self.help) + ";"
 		line += str(self.outofcontext) + ";"
-		line += str(self.errors) + "\n"
+		line += str(self.errors) + ";"
+		line += str(self.admin) + "\n"
 		return line
+
+	def list_to_csv_format(self, data):
+		m = ""
+		for d in data:
+			m += str(d) + ";"
+		return m
+
+	#Checking last new data date...
+	def check_data_interval(self):
+		if dt.today() - self.last_save > timedelta(hours=24, minutes=0):
+			self.save_usage()
+			self.reset()
 
 	#Registering a new start command...
 	def add_start(self):
+		self.check_data_interval()
 		self.start += 1
 
 	#Registering a new pcs...
 	def add_pcs(self, key):
+		self.check_data_interval()
 		self.pcs[key] += 1
 	
 	#Registering a new chain...
 	def add_chain(self, key):
+		self.check_data_interval()
 		self.chain[key] += 1
+
+	#Registering a new matrix...
+	def add_matrix(self, key):
+		self.check_data_interval()
+		self.matrix[key] += 1
+
+	#Registering a new random...
+	def add_random(self, key):
+		self.check_data_interval()
+		self.random[key] += 1
 	
 	#Registering a new error report...
 	def add_error_report(self):
+		self.check_data_interval()
 		self.error_reports += 1
 
 	#Registering a new language...
 	def add_language(self, l):
+		self.check_data_interval()
 		self.language[l] += 1
 
+#Registering a new privacy...
+	def add_privacy(self):
+		self.check_data_interval()
+		self.privacy += 1
+	
 	#Registering a new help...
 	def add_help(self):
+		self.check_data_interval()
 		self.help += 1
+
+	#Registering a new admin...
+	def add_admin(self):
+		self.check_data_interval()
+		self.admin += 1
 	
 	#Registering an out of context message...
 	def add_outofcontext(self):
+		self.check_data_interval()
 		self.outofcontext += 1
 
 	#Registering a new error...
 	def add_error(self):
+		self.check_data_interval()
 		self.errors += 1
 	
 	#Saving en error report...
 	def save_error_report(self, command, description, user):
 		file = open(self.errors_path, "a")
-		t = dt.datetime.now()
+		t = dt.now()
 		date = str(t.year) + "-" + str(t.month) + "-" + str(t.day)
 		file.write(date)
 		file.write(command)
---
+++ users.py
@@ -0,0 +1,30 @@
+import datetime as dt
+import json as js
+
+class Users():
+    "The class to save user's data..."
+
+    def __init__(self, output_path):
+        self.output_path = output_path
+
+    #Saving the user's data...
+    def save_user_data(self, id, the_data):
+        file = open(self.output_path + str(id) + ".json", "w")
+        js.dump(the_data, file)
+        file.close()
+
+    #To recover the user's data...
+    def load_user_data(self, id, chat_data):
+        try:
+            the_data = js.load(open(self.output_path + str(id) + ".json", "r"))
+            for key in the_data:
+                chat_data[key] = the_data[key]
+        except:
+            pass
+
+    #Printing Users()...
+    def __str__(self):
+        return "- musiCal Bot\n" + \
+                "  I am the class in charge of working with user's data...\n" + \
+                "  gitlab.com/musicaltools/caltoolsbot\n" + \
+                "  rodrigovalla@protonmail.ch"
